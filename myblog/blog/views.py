feeeade19e75a7e443ae20c1390aca2a50ec40f0
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2023-12-06 22:53:23 +0400

    add 2

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def blog_view(request):
-    blog_titles = ['Blog post 1', 'Blog post 2', 'Blog post 3']
+    blog_titles = ['Blog post 1', 'Blog post 2', 'Blog post 3', 'Blog post 4']
 
     data = {"blog_names" : blog_titles}
     
