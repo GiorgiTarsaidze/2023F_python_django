cb0c9795dd9667bdb584b03d75185c9ae1d8c4a0
GiorgiTarsaidze <giorgi.tarsaidze.1@btu.edu.ge>
2024-02-01 01:49:04 +0400

    add projects

--- zoo/zoo/urls.py
+++ zoo/zoo/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from library import views
 
 urlpatterns = [
+    path('', include("zookeeper.urls")),
     path('admin/', admin.site.urls),
-    path('', views.book_list),
-    path('library/<int:book_id>', views.book_detail, name='book_id')
 ]
